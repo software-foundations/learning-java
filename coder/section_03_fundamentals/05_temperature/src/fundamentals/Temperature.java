1fa574bf595899062261ecfe6a357143ab9874b7
brunoconde07 <brunoconde.ufpa@gmail.com>
2022-08-04 00:32:51 -0300

    [coder] -> class 22

--- coder/section_03_fundamentals/05_temperature/src/fundamentals/Temperature.java
+++ coder/section_03_fundamentals/05_temperature/src/fundamentals/Temperature.java
@@ -1,9 +1,28 @@
 package fundamentals;
 
+/**
+ * This class cast the temperature from °F to °C
+ * 
+ * @author Bruno Conde <brunoconde.ufpa@email.com>
+*/
 public class Temperature {
 
-	public static void main (string[] args) {
+	public static void main (String[] args) {
 
-		
+		final double FACTOR = 5.0/9.0;
+
+		final double ADJUSTMENT = 32;
+
+		double fahrenheit = 86;
+
+		double celcius = (fahrenheit - ADJUSTMENT) * FACTOR;
+
+		System.out.println(celcius + " °C");
+
+		fahrenheit = 150;
+
+		celcius = (fahrenheit - ADJUSTMENT) * FACTOR;
+
+		System.out.println(celcius + " °C");
 	}
 }
